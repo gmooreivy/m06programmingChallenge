dea7aa55f20b550254afedb8145035d8aa3b3016
gmooreivy <gmoore138@ivytech.edu>
2025-09-30 12:07:28 -0400

    Initial Commit

---
+++ 15-1.py
@@ -0,0 +1,17 @@
+import random
+import time
+import multiprocessing as mp
+from multiprocessing import Process
+import os
+timeCount = 3
+
+def printTime(timeout) :
+    print('printing time...')
+    time.sleep(timeout)
+    print(time.ctime(time.time()))
+
+if __name__ == '__main__':
+    for i in range(timeCount):
+        timeout = random.random()
+        p = mp.Process(target=printTime, args=(timeout,))
+        p.start()
